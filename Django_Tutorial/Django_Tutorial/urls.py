b9a8406135aab60617966d6c901c20e5ee428db3
logue115 <37748129+logue115@users.noreply.github.com>
2019-04-05 12:51:42 -0400

    Changed views.py and root urls.py

--- Django_Tutorial/Django_Tutorial/urls.py
+++ Django_Tutorial/Django_Tutorial/urls.py
@@ -3,12 +3,15 @@ Definition of urls for Django_Tutorial.
 """
 
 from django.conf.urls import include, url
+import HelloDjangoApp.views
+
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = [
+
     # Examples:
     # url(r'^$', Django_Tutorial.views.home, name='home'),
     # url(r'^Django_Tutorial/', include('Django_Tutorial.Django_Tutorial.urls')),
@@ -18,4 +21,7 @@ urlpatterns = [
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', HelloDjangoApp.views.index, name='index'),
+    url(r'^home$', HelloDjangoApp.views.index, name='home'),
 ]
