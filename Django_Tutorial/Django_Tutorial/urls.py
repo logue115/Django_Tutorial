cb864270c9c06dfa0352f79d848f0c518b1ccb2a
logue <logue115@hotmail.com>
2019-04-07 01:38:42 -0400

    add layout.html

--- Django_Tutorial/Django_Tutorial/urls.py
+++ Django_Tutorial/Django_Tutorial/urls.py
@@ -13,9 +13,7 @@ import HelloDjangoApp.views
 urlpatterns = [
     
     url(r'^$', HelloDjangoApp.views.index, name='index'),
-    url(r'^home$', HelloDjangoApp.views.home, name='home'),
-    url(r'^time$', HelloDjangoApp.views.time, name='time'),
-    url(r'^time_template$', HelloDjangoApp.views.time_template, name='time_template'),
+    url(r'^home$', HelloDjangoApp.views.index, name='home'),
     url(r'^about$', HelloDjangoApp.views.about, name='about'),
     
 
--- Django_Tutorial/HelloDjangoApp/views.py
+++ Django_Tutorial/HelloDjangoApp/views.py
@@ -2,22 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
 
+
 def index(request):
-    return HttpResponse("Hello, Django!")
-
-def home(request):
-    return HttpResponse("Hello, Django Home!")
-
-def time(request):
-    now=datetime.now()
-
-    html_content = "<html><head><title>Hello, Django</title></head><body>"
-    html_content += "<strong>Hello Django!</strong> on" + now.strftime("%A, %d %B, %Y at %X")
-    html_content += "</body></html>"
-
-    return HttpResponse(html_content)
-
-def time_template(request):
     now=datetime.now()
 
     return render(
